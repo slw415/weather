4ada370d35ba454c389a6e4d6bb91cfb4adfee99
slw415 <17858024722@163.com>
2021-06-08 20:25:20 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Shishuaishuai\Weather\Exceptions;
+
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Shishuaishuai\Weather\Exceptions;
+
+
+class HttpException extends Exception
+{
+
+}
